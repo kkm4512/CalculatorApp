a775560b5387f15bcc5fcb5e635ed8e409cd2436
Robbie <robbie331@>
2024-07-24 14:30:10 +0900

    Level 2 요구사항 - 1번 clear 변경사항 : 1. calculate 메서드를 생성한다. 2. 피연산자 (x,y)와 연산자 (operator)를 받아 사칙연산후, 결과값을 return 하는 함수를 만들 수 있다. 3. 컬렉션타입의 인스턴스 변수를 선언 할 수 있다. 4. 연산자의 불일치, 분모가 0이 들어왔을 경우에 대한 exception 처리를 할 수 있다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,36 @@
 package calculator;
 
-public class Calculator {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Calculator {
+    /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
+    List<Integer> results = new ArrayList<>();
+
+    public int calculate(int x, String operator, int y) throws Exception {
+        //연산자가 잘 들어왔는지, 분모가 0인지 체크후 익셉션처리
+        try {
+            //연산자 확인
+            if (operator.equals("+") && operator.equals("-") && operator.equals("*") && operator.equals("/"))
+                throw new HandleInputMisMatchException(operator);
+            //분모확인
+            if (y == 0) throw new HandleArithmeticException(y);
+
+        } catch (HandleInputMisMatchException e) {
+            System.out.println(e.getMessage());
+            //깅제 종료
+            return -9999;
+        } catch (HandleArithmeticException e) {
+            System.out.println(e.getMessage());
+            //깅제 종료
+            return -9999;
+        }
+        //문제없으면 연산 수행
+        int result = 0;
+        if (operator.equals("+")) result = x + y;
+        else if (operator.equals("-")) result = x - y;
+        else if (operator.equals("*")) result = x * y;
+        else result = x / y;
+        return result;
+    }
 }
