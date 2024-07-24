81f87f4210aa4c2d41421f2a1b8c4e961bb3a5ba
Robbie <robbie331@>
2024-07-24 16:56:11 +0900

    Level 2 요구사항 - 7번 clear 변경사항 : 1. App 클래스에서 scanner에 따라 원의넓이,사칙연산 을 구할 수 있습니다 2. 원의 넓이를 구하는 메서드를 Calculator 클래스에서 구현합니다 (static 미선언, final 선언) 3. 원의 넓이 결과를 저장하는 컬렉션 타입,getter,setter,외부 접근 불가 구현

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,45 +6,53 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
+        System.out.println("원의넓이를 구하시려면 원의넓이, 사칙연산을 수행하시려면 사칙연산을 기재하세요");
+        String firstInput = sc.nextLine();
 
 
         //while 문 제어 초기값
         boolean flag = true;
 
-        while (flag) {
-            //첫번째 숫자
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int x = sc.nextInt();
+        if (firstInput.equals("사칙연산")) {
+            while (flag) {
+                //첫번째 숫자
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int x = sc.nextInt();
 
-            //연산자
-            //nextInt() 같은경우 끝에 개행문자가 포함되기때문에, 바로 nextLine() 찍으면 공백만 찍혀서, 한번 미리 써주고 개행문자 스킵
-            sc.nextLine();
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            String operator = sc.nextLine();
+                //연산자
+                //nextInt() 같은경우 끝에 개행문자가 포함되기때문에, 바로 nextLine() 찍으면 공백만 찍혀서, 한번 미리 써주고 개행문자 스킵
+                sc.nextLine();
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                String operator = sc.nextLine();
 
-            //두번째 숫자
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int y = sc.nextInt();
-            sc.nextLine();
-            try {
-                int result = calculator.calculate(x, operator, y);
-                //하나씩 저장
-                calculator.setResult(result);
-            } finally {
-                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-                String input = sc.nextLine();
-                switch (input) {
-                    case "exit":
-                        flag = !flag;
-                        break;
-                    case "remove":
-                        calculator.deleteResult();
-                        break;
-                    case "inquiry":
-                        System.out.println(calculator.getAllResults());
-                        break;
+                //두번째 숫자
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int y = sc.nextInt();
+                sc.nextLine();
+                try {
+                    int result = calculator.calculate(x, operator, y);
+                    //하나씩 저장
+                    calculator.setResult(result);
+                } finally {
+                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                    String input = sc.nextLine();
+                    switch (input) {
+                        case "exit":
+                            flag = !flag;
+                            break;
+                        case "remove":
+                            calculator.deleteResult();
+                            break;
+                        case "inquiry":
+                            System.out.println(calculator.getAllResults());
+                            break;
+                    }
                 }
             }
+        } else if (firstInput.equals("원의넓이")) {
+            System.out.println("원의 반지름값을 기재해주세요");
+            double radius = sc.nextDouble();
+            System.out.println(calculator.calculateCircleArea(radius));
         }
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,12 +1,16 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class Calculator {
     /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
     //초기화 시켜주는 부분
-    private Queue<Integer> results = new LinkedList<>();
+    private final Queue<Integer> results = new LinkedList<>();
+    /* 원의 넓이를 저장하는 컬렉션 타입 필드 선언 및 생성 */
+    private final List<Double> circleAreas = new ArrayList<>();
 
     //조회
     public Queue<Integer> getAllResults() {
@@ -24,6 +28,17 @@ public class Calculator {
     }
 
 
+    //조회
+    public List<Double> getCircleAreas() {
+        return circleAreas;
+    }
+
+    //추가
+    public void setCircleAreas(double circleArea) {
+        circleAreas.add(circleArea);
+    }
+
+    //사칙연산 수행
     public int calculate(int x, String operator, int y) throws Exception {
         //연산자가 잘 들어왔는지, 분모가 0인지 체크후 익셉션처리
         try {
@@ -49,5 +64,19 @@ public class Calculator {
         else if (operator.equals("*")) result = x * y;
         else result = x / y;
         return result;
+    }
+
+    //원의 반지름 넓이 구하기
+    //3.14를 곱해야하기때문에 타입 double 선언
+
+    /**
+     * static 미선언
+     * 1. 이미 App 클래스에서, 사칙연산을 수행하기위해 인스턴스를 생성하였기떄문에, 클래스로드 시점에 사용되는 static 메서드의 이점을 활용 하기 어려움
+     * final 선언
+     * 1. 오버라이딩을 불가능하게 하여, 하위 클래스들에서도, 동일하게 사용가능하게 하기 위함
+     */
+
+    public final double calculateCircleArea(double radius) {
+        return 3.14 * Math.pow(radius, 2);
     }
 }
