4fd237fdfa0f51d9fc00d73cafc6ff7d6bb58588
Robbie <robbie331@>
2024-07-24 11:45:57 +0900

    Level 1 요구사항 - 1번 clear 변경사항 : 1. x,y로 피연산자를 받을 수 있다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import java.util.*;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello World!");
+        Scanner sc = new Scanner(System.in);
+
+            //첫번째 숫자
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int x = sc.nextInt();
+
+            //두번째 숫자
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int y = sc.nextInt();
+            sc.nextLine();
     }
 }
